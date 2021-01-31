3b377a3fdc46e5767bd5dfe73579fd37da0716ab
elunacado <54047744+elunacado@users.noreply.github.com>
2021-01-30 22:01:24 -0600

    Añadi al player

--- Game/main.py
+++ Game/main.py
@@ -4,12 +4,20 @@ import pygame
 pygame.init()
 
 # create screen
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((600,600))
 
 #Title & Icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('project.png')
 pygame.display.set_icon(icon)
+
+#Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY= 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
 
 # Game Loop so the window wont close
 running = True
@@ -19,5 +27,7 @@ while running:
            running = False
 
     #RGB - Red-Green-Blue
-    screen.fill((0, 255, 0))
+    screen.fill((0, 0, 0))
+
+    player()
     pygame.display.update()
